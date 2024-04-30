fd0bbc762148a02b649d813b8133122ede6e850a
jjl20011 <jjl20011@datasec.engr.uconn.edu>
2024-04-29 20:21:59 -0400

    updated repo structure

---
+++ build.sh
@@ -0,0 +1,5 @@
+CUR_DIR=$(pwd)
+cd honors
+./f1_analysis.sh
+cd "$CUR_DIR"
+quarto render
--- honors/f1_analysis.sh
+++ honors/f1_analysis.sh
@@ -1,7 +1,4 @@
 # make sure to chmod +x script.sh before running this file
 python3 run_analysis.py
 python3 write_quarto.py
-quarto render outcomes.qmd
-git add .
-git commit -m "made predictions for most recent weekend"
-git push
+quarto render
