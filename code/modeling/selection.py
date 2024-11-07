cdee59f3da74ef3cd7b61a8d212698ed6092d30e
Nicholas Pfeifer <nickpfeifer5518@gmail.com>
2024-11-06 19:01:10 -0500

    comments in selection.py

--- code/modeling/selection.py
+++ code/modeling/selection.py
@@ -6,3 +6,19 @@ def Josh():
 # git add .
 # git commit -m "message"
 # git push
+
+
+### Making environments in Conda
+# conda create -n env_name
+# conda activate env_name
+# conda deactivate
+
+# conda install package_name
+
+
+
+# Finding best window function
+# for i in range(n)
+# select features and create model using n prior races of data
+# find f1_score and accuracy
+# output table of results
