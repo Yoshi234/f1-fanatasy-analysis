41df3180f7ec9d38a780eb00000c55b5bc1063fd
jjl20011 <jjl20011@ds.stat.uconn.edu>
2024-11-18 13:52:08 -0500

    updates to models_by_window_2 to make adaptable to 2024 data

--- code/modeling/models_by_window.py
+++ code/modeling/models_by_window.py
@@ -7,7 +7,14 @@ import numpy as np
 from sklearn.metrics import f1_score
 from tqdm import tqdm
 
-def models_by_window_2(start_yr, start_r, n):
+def models_by_window_2(
+    start_yr, 
+    start_r, 
+    n, 
+    data='../../data/clean_model_data.feather', 
+    max_year=2024, 
+    debug=False
+):
     """
     Summarizes accuracy and f1 score for each window across all races in span
         
@@ -15,6 +22,7 @@ def models_by_window_2(start_yr, start_r, n):
     - start_yr - the first year of the span on which to test
     - start_r -- the first round to be tested on in start_yr
     - n -------- the biggest window size to test; windows from size 1 to n will be tested
+    - data ----- data file to choose
 
     Under the hood:
     - The function consists of 3 nested for loops (n, year, round)
@@ -26,7 +34,10 @@ def models_by_window_2(start_yr, start_r, n):
     the average accuracy and f1_score for that value of n is added to a different dataframe
     - After all iterations of n, the dataframe containing the averages is returned
     """
-    all_data = pd.read_feather("../../data/clean_model_data.feather")
+    if data.split(".")[-1] == "feather":
+        all_data = pd.read_feather(data)
+    elif data.split(".")[-1] == 'csv':
+        all_data = pd.read_csv(data)
 
     results_df = pd.DataFrame(columns=['n', 'Accuracy', 'F1 Score'])
 
@@ -36,7 +47,7 @@ def models_by_window_2(start_yr, start_r, n):
         single_n_results_df = pd.DataFrame(columns=['Accuracy', 'F1 Score'])
 
         # for race from start_yr, start_r to most recent race
-        for year in range(start_yr, 2024):
+        for year in range(start_yr, max_year):
             if year == start_yr:
                 num_rounds = len(all_data[all_data['year'] == year]['round'].unique())
                 first_r = start_r 
@@ -49,9 +60,9 @@ def models_by_window_2(start_yr, start_r, n):
 
             for round in range(first_r, num_rounds+1):
                 # print(i, year, round) # just a test
-
+                
                 # get data window of current size and current race
-                train_window = get_data_in_window(k = i+1, yr = year, r_val = round)
+                train_window = get_data_in_window(k = i+1, yr = year, r_val = round, track_dat=all_data)
                 train_window['Podium Finish'] = ['Yes' if position <= 3 else 'No' for position in train_window['positionOrder']]
                 train_window = train_window.reset_index().drop(['index'],axis=1)
                 
@@ -66,6 +77,9 @@ def models_by_window_2(start_yr, start_r, n):
                 #print(train_window['round'].unique()) # another test
 
                 # select features using the data window
+                if debug: 
+                    print("[DEBUG]: train_window.keys() = \n{}".format(train_window.keys()))
+                
                 train_features = train_window[['Podium Finish', 'constructorId_9']]
 
                 features = train_features.columns.drop(['Podium Finish'])
@@ -83,11 +97,17 @@ def models_by_window_2(start_yr, start_r, n):
                 probabilities = lr_results.predict(test)
                 
                 # classify predictions
-                labels = np.array(['Yes'] * 20)
-                no_indices = np.argpartition(probabilities, 17)[:17]
+                n_outs = test_window.shape[0]
+                labels = np.array(['Yes'] * n_outs) # match num yesses to num outputs
+                no_indices = np.argpartition(probabilities, n_outs-3)[:n_outs-3] # where n_outs = 20, index is 17
                 labels[no_indices] = 'No'
                 
                 # Find accuracy and f1 score
+                if debug: 
+                    print("[DEBUG]: labels.shape = {}".format(labels.shape))
+                    print("[DEBUG]: test_window['Podium Finish'].shape = {}".format(test_window['Podium Finish'].shape))
+                    print("[DEBUG]: test_window['driverId'].unique() = {}".format(test_window['driverId'].unique()))
+                
                 test_accuracy = np.mean(labels == test_window['Podium Finish'])
                 test_f1 = f1_score(np.array(test_window['Podium Finish']), labels, pos_label='Yes')
 
